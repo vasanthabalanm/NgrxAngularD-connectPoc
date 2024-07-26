d8ce6a97ec4e3c377ed95e008179ec0c5442e467
vasanthabalanm <vasanthabalanm.kanini@gmail.com>
2024-07-26 11:46:06 +0530

    updated

--- src/app/helpers/Formvalidators.ts
+++ src/app/helpers/Formvalidators.ts
@@ -3,6 +3,5 @@ export const Formvalidators = {
     firstName: /^[a-zA-Z\s]*$/,
     lastName: /^[a-zA-Z\s]*$/,
     mail: /^[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z.]{2,6}$/,
-    phone: /^\+\d{2,3}-\d{10}$/,
-    role: /^[0-9a-zA-Z-]*$/,
+    phone: /^\+\d{2,3}-\d{10}$/
 };
